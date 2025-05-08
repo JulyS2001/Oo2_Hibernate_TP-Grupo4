8d2dbaf4a74e72ae8b2933d594d756da5b74e75d
JulyS <rukysorli@gmail.com>
2025-05-08 16:00:54 -0300

    Creacion de la clase UserAbm.java

--- src/negocio/UserAbm.java
+++ src/negocio/UserAbm.java
@@ -1,5 +1,41 @@
 package negocio;
 
-public class UserAbm {
+import java.util.List;
+import dao.UserDao;
+import datos.User;
+import datos.Receptor;
+import datos.Emisor;
 
+public class UserAbm {
+	
+	private static UserAbm instancia = null; 
+	private UserDao dao; 
+	protected UserAbm() {
+		dao = UserDao.getInstance();
+	}
+	
+	public static UserAbm getInstance() {
+		if(instancia == null)
+			instancia = new UserAbm();
+		return instancia; 
+	}
+	
+	public int crearUser(User u) {
+		return dao.crearUser(u);
+	}
+	
+	public void eliminarUser(User u) {
+		dao.eliminarUser(u);
+	}
+	
+	public void modificarUser(User u) {
+		dao.modificarUser(u);
+	}
+	
+	public User traerUser(int idUser) {
+		return dao.traer(idUser);
+	}
+	
+    
 }
+	
