7f05844055ce68b9bbd24bcb0f38943c41025721
Anahimm <ana_maitu06@hotmail.com>
2025-05-08 14:04:18 -0300

    ABM de Categoria y Prioridad
    Creacion de las clases abm de Categoria y Prioridad dentro del package negocio

--- src/negocio/CategoriaAbm.java
+++ src/negocio/CategoriaAbm.java
@@ -1,5 +1,54 @@
 package negocio;
 
+import dao.HibernateUtil;
+import datos.Categoria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 public class CategoriaAbm {
 
+    public void agregarCategoria(Categoria categoria) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(categoria);
+        tx.commit();
+        session.close();
+    }
+
+    public void modificarCategoria(Categoria categoria) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(categoria);
+        tx.commit();
+        session.close();
+    }
+
+    public void eliminarCategoria(long idCategoria) throws Exception {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Categoria categoria = session.get(Categoria.class, idCategoria);
+
+        // Validar si está relacionada a tickets
+        Query<Long> query = session.createQuery(
+            "select count(*) from Ticket t where t.categoria.id = :id", Long.class);
+        query.setParameter("id", idCategoria);
+        Long cantidad = query.uniqueResult();
+
+        if (cantidad > 0) {
+            session.close();
+            throw new Exception("No se puede eliminar la categoría porque está asociada a tickets.");
+        }
+
+        session.delete(categoria);
+        tx.commit();
+        session.close();
+    }
+
+    public Categoria traerCategoria(long idCategoria) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Categoria categoria = session.get(Categoria.class, idCategoria);
+        session.close();
+        return categoria;
+    }
 }
--- src/negocio/PrioridadAbm.java
+++ src/negocio/PrioridadAbm.java
@@ -1,5 +1,41 @@
 package negocio;
 
+import dao.HibernateUtil;
+import datos.Prioridad;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class PrioridadAbm {
 
+    public void agregarPrioridad(Prioridad prioridad) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(prioridad);
+        tx.commit();
+        session.close();
+    }
+
+    public void modificarPrioridad(Prioridad prioridad) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(prioridad);
+        tx.commit();
+        session.close();
+    }
+
+    public void eliminarPrioridad(long idPrioridad) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Prioridad prioridad = session.get(Prioridad.class, idPrioridad);
+        session.delete(prioridad);
+        tx.commit();
+        session.close();
+    }
+
+    public Prioridad traerPrioridad(long idPrioridad) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Prioridad prioridad = session.get(Prioridad.class, idPrioridad);
+        session.close();
+        return prioridad;
+    }
 }
