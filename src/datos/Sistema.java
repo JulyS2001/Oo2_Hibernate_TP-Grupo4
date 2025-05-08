296e7dae04d8430cf97b9f4a7b4035ce65eecec8
Axel De Pietro <axel.dp44@gmail.com>
2025-05-07 23:22:33 -0300

    calse sistema en datos

--- src/datos/Sistema.java
+++ src/datos/Sistema.java
@@ -2,4 +2,24 @@ package datos;
 
 public class Sistema {
 
+private int idSistema;
+	
+	public Sistema() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getIdSistema() {
+		return idSistema;
+	}
+
+	protected void setIdSistema(int idSistema) {
+		this.idSistema = idSistema;
+	}
+
+	@Override
+	public String toString() {
+		return "SistemaDao [idSistema=" + idSistema + "]";
+	}
+	
+	
 }
