eafc5783ce2d078b4391f3dbd83c614afc208450
MarcosTambosco <marcostambosco@gmail.com>
2025-05-07 23:46:45 -0300

    Clases user y ticket

--- src/datos/Ticket.java
+++ src/datos/Ticket.java
@@ -1,5 +1,130 @@
 package datos;
 
+import java.util.Date;
+import java.util.Set;
+
 public class Ticket {
 
+    private int idTicket;
+
+    private String titulo;
+
+    private String descripcion;
+
+    private Date fecha_Creacion;
+
+    private Date fecha_Cierre;
+    
+   
+    private Categoria categoria;
+
+  
+    private Estado estado;
+
+    
+    private Prioridad prioridad;
+
+    
+    private Sistema sistema;
+  
+
+    public Ticket() {
+    }
+    
+    
+
+    public Ticket(int idTicket, String titulo, String descripcion, Date fecha_Creacion, Date fecha_Cierre) {
+		super();
+		this.idTicket = idTicket;
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.fecha_Creacion = fecha_Creacion;
+		this.fecha_Cierre = fecha_Cierre;
+	}
+
+
+
+	// Getters y Setters
+
+    public int getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(int idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha_Creacion() {
+        return fecha_Creacion;
+    }
+
+    public void setFecha_Creacion(Date fecha_Creacion) {
+        this.fecha_Creacion = fecha_Creacion;
+    }
+
+    public Date getFecha_Cierre() {
+        return fecha_Cierre;
+    }
+
+    public void setFecha_Cierre(Date fecha_Cierre) {
+        this.fecha_Cierre = fecha_Cierre;
+    }
+    
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Prioridad getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Prioridad prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public Sistema getSistema() {
+        return sistema;
+    }
+
+    public void setSistema(Sistema sistema) {
+        this.sistema = sistema;
+    }
+
+
+	@Override
+	public String toString() {
+		return "Ticket [idTicket=" + idTicket + ", titulo=" + titulo + ", descripcion=" + descripcion
+				+ ", fecha_Creacion=" + fecha_Creacion + ", fecha_Cierre=" + fecha_Cierre 
+				+ "]";
+	}
+    
+    
 }
