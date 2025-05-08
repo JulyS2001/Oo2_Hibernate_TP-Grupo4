b4caf0d0dbb02844f130d28d578cebe640c88e09
Anahimm <ana_maitu06@hotmail.com>
2025-05-07 23:40:37 -0300

    Categoria y Prioridad

--- src/datos/Categoria.java
+++ src/datos/Categoria.java
@@ -1,5 +1,31 @@
 package datos;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class Categoria {
+private long idCategoria;
+private String nombre;
+
+
+public Categoria() {}
+
+public Categoria (String nombre) {
+	this.nombre = nombre;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void SetNombre (String nombre) {
+	this.nombre = nombre;
+}
+
+@Override
+public String toString() {
+	return "Categoria [idCategoria=" + idCategoria + ", nombre=" + nombre + "]";
+}
+
 
 }
--- src/datos/Prioridad.java
+++ src/datos/Prioridad.java
@@ -1,5 +1,32 @@
 package datos;
 
-public class Prioridad {
+import java.util.Set;
+import java.util.HashSet;
 
-}
+public class Prioridad {
+	private long idPrioridad;
+	private String nombre;
+
+
+	public Prioridad() {}
+
+	public Prioridad (String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void SetNombre (String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Prioridad [idPioridad=" + idPrioridad + ", nombre=" + nombre + "]";
+	}
+
+
+	}
+
